1919c38e421a5e02e820ffb515663780fa2fe9d6
Asher Stern <astern7@gmail.com>
2022-08-06 15:16:25 +0300

    create app.py

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app! Change it!"
+
+
+if __name__ == "__main__":
+    app.run()
+
