6fb34f979fcf9b7753545395d90399f8132ede50
Asher Stern <astern7@gmail.com>
2022-08-06 15:46:15 +0300

    update

--- src/app.py
+++ src/app.py
@@ -4,9 +4,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
+class AppContents(object):
+    def __init__(self):
+        with open('html/index.html') as f:
+            self.index_contents = f.read()
+        with open('html/result.html') as f:
+            self.result = f.read()
+
+
+app_contents = AppContents()
+
+
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app! Change it!"
+    return app_contents.index_contents
+
+
+@app.route("/result")
+def result():
+    return app_contents.index_contents
 
 
 if __name__ == "__main__":
---
+++ src/n1.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    with open('html/index.html') as f:
+        index_contents = f.read()
+
+    print(index_contents)
+
+
+if __name__ == '__main__':
+    main()
