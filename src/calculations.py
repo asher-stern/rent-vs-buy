5eed21e89f14782b58b02fba59f6c6fbbc8818c6
Asher Stern <astern7@gmail.com>
2022-08-12 20:44:53 +0300

    Work on increase in rent

--- src/calculations.py
+++ src/calculations.py
@@ -1,5 +1,10 @@
 
-from extended_calculations import convert_annual_nominal_interest_rate_to_monthly_effective, increase_and_depreciation_calculation
+from extended_calculations import \
+    convert_annual_nominal_interest_rate_to_monthly_effective, \
+    increase_and_depreciation_calculation, \
+    create_list_of_rent_payments, create_lists_of_savings, calculate_saving_with_detailed_allowance
+
+
 
 
 def calc_saving(initial, monthly, months, annual_rate_percent):
@@ -19,10 +24,12 @@ def calculate_spitzer(loan, mortgage_rate_percent, months):
 
 class Calculate(object):
     def __init__(self, apartment, immediate_payment, rent_monthly, years, mortgage_rate, saving_rate,
-                 annual_increase_apartment_percent, building_worth, annual_depreciation_percent
+                 annual_increase_apartment_percent, building_worth, annual_depreciation_percent,
+                 annual_increase_rent_percent
                  ):
         self.apartment, self.immediate_payment, self.rent_monthly, self.years, self.mortgage_rate, self.saving_rate = apartment, immediate_payment, rent_monthly, years, mortgage_rate, saving_rate
         self.annual_increase_apartment_percent, self.building_worth, self.annual_depreciation_percent = annual_increase_apartment_percent, building_worth, annual_depreciation_percent
+        self.annual_increase_rent_percent = annual_increase_rent_percent
         self.months = years * 12.0
 
         self.mortgage_payment = None
@@ -31,8 +38,7 @@ class Calculate(object):
         self.total_saving_if_buy = None
         self.apartment_value_at_end = None
         self.total_if_buy = None
-        self.monthly_saving = None
-        self.rent_smaller_than_mortgage = True
+        self.first_month_rent_no_saving, self.first_month_buy_saving, self.saving_list_if_buy, self.saving_list_if_rent = None, None, None, None
 
     def calculate(self):
         loan = self.apartment - self.immediate_payment
@@ -40,18 +46,32 @@ class Calculate(object):
         self.total_cost_of_apartment = self.immediate_payment + self.mortgage_payment * self.months
         self.apartment_value_at_end = self._calculate_apartment_value_at_end()
 
-        initial_saving = self.immediate_payment
-        self.monthly_saving = self.mortgage_payment - self.rent_monthly
+        list_of_rent_payments = create_list_of_rent_payments(self.rent_monthly, self.annual_increase_rent_percent / 12.0, int(self.months))
+        self.first_month_rent_no_saving, self.first_month_buy_saving, self.saving_list_if_buy, self.saving_list_if_rent = create_lists_of_savings(self.mortgage_payment, list_of_rent_payments)
+        saving_monthly_effective_rate = convert_annual_nominal_interest_rate_to_monthly_effective(self.saving_rate / 100.0)
+        self.total_saving_if_rent = calculate_saving_with_detailed_allowance(self.immediate_payment, self.saving_list_if_rent, saving_monthly_effective_rate, int(self.months))
+        self.total_saving_if_buy = calculate_saving_with_detailed_allowance(0.0, self.saving_list_if_buy, saving_monthly_effective_rate, int(self.months))
+        self.total_if_buy = self.apartment_value_at_end + self.total_saving_if_buy
 
-        if self.monthly_saving >= 0:
-            self.rent_smaller_than_mortgage = True
-            self.total_saving_if_rent = calc_saving(initial_saving, self.monthly_saving, self.months, self.saving_rate)
-            self.total_if_buy = self.apartment_value_at_end
-        else:
-            self.rent_smaller_than_mortgage = False
-            self.total_saving_if_rent = calc_saving(initial_saving, 0.0, self.months, self.saving_rate)
-            self.total_saving_if_buy = calc_saving(0.0, -self.monthly_saving, self.months, self.saving_rate)
-            self.total_if_buy = self.apartment_value_at_end + self.total_saving_if_buy
+
+
+
+
+        #
+        #
+        #
+        #
+        # self.monthly_saving = self.mortgage_payment - self.rent_monthly
+        #
+        # if self.monthly_saving >= 0:
+        #     self.rent_smaller_than_mortgage = True
+        #     self.total_saving_if_rent = calc_saving(initial_saving, self.monthly_saving, self.months, self.saving_rate)
+        #     self.total_if_buy = self.apartment_value_at_end
+        # else:
+        #     self.rent_smaller_than_mortgage = False
+        #     self.total_saving_if_rent = calc_saving(initial_saving, 0.0, self.months, self.saving_rate)
+        #     self.total_saving_if_buy = calc_saving(0.0, -self.monthly_saving, self.months, self.saving_rate)
+        #     self.total_if_buy = self.apartment_value_at_end + self.total_saving_if_buy
 
     def _calculate_apartment_value_at_end(self):
         # self.annual_increase_apartment_percent, self.building_worth, self.annual_depreciation_percent
--- src/extended_calculations.py
+++ src/extended_calculations.py
@@ -1,6 +1,4 @@
 
-import sys
-
 def convert_annual_nominal_interest_rate_to_monthly_effective(rate_fraction):
     return (1.0 + rate_fraction) ** (1.0 / 12.0) - 1.0
 
@@ -17,16 +15,10 @@ def increase_and_depreciation_calculation(asset_total_value,
     one_plus_monthly_increase = 1.0 + monthly_increase
     one_minus_monthly_depreciation = 1.0 - monthly_depreciation
 
-    print('-----')
     for _ in range(months):
         not_depreciated *= one_plus_monthly_increase
         depreciated *= one_plus_monthly_increase
         depreciated *= one_minus_monthly_depreciation
-        print(not_depreciated, '\t', depreciated)
-
-    print('-----')
-    print()
-    sys.stdout.flush()
 
     return not_depreciated + depreciated
 
@@ -40,12 +32,37 @@ def calculate_saving_with_detailed_allowance(base_value, list_of_allowances, mon
     return value
 
 
-def create_list_of_rate_payments(initial_value, annual_increase_fraction, months):
+def create_list_of_rent_payments(initial_value, annual_increase_fraction, months):
     list_of_payments = list()
     one_plus_increase = 1.0 + annual_increase_fraction
     value = initial_value
-    for month in months:
+    for month in range(months):
         if month > 0 and (months % 12) == 0:
             value *= one_plus_increase
         list_of_payments.append(value)
     return list_of_payments
+
+
+def create_lists_of_savings(mortgage_payment, list_of_rent_payments):
+    first_month_rent_no_saving = None
+    first_month_buy_saving = None
+    saving_if_buy = list()
+    saving_if_rent = list()
+    for month, rent_payment in enumerate(list_of_rent_payments):
+        if first_month_rent_no_saving is None and rent_payment >= mortgage_payment:
+            first_month_rent_no_saving = month
+        if first_month_buy_saving is None and mortgage_payment < rent_payment:
+            first_month_buy_saving = month
+        if mortgage_payment > rent_payment:
+            saving_if_buy.append(0.0)
+            saving_if_rent.append(mortgage_payment - rent_payment)
+        elif mortgage_payment < rent_payment:
+            saving_if_buy.append(rent_payment - mortgage_payment)
+            saving_if_rent.append(0.0)
+        else:
+            saving_if_buy.append(0.0)
+            saving_if_rent.append(0.0)
+
+    return first_month_rent_no_saving, first_month_buy_saving, saving_if_buy, saving_if_rent
+
+
